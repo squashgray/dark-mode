3079bb7265e48a782f0465c289c86cd90d300920
squashgray <squashgray@gmail.com>
2019-12-03 16:44:35 -0800

    useLocalStorage

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [storedValue, setValue] = useLocalStorage();
+
+  useEffect();
+};
